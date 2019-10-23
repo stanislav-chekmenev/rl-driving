660a73ef11a179aa9a2317f3e789e7b14d2f3478
stanislav-chekmenev <stanislav.chekmenev@gmail.com>
2019-10-23 15:23:19 +0200

    hello_pybullet understood

--- hello_pybullet.py
+++ hello_pybullet.py
@@ -1,10 +1,10 @@
 import pybullet as p
 import time
 import pybullet_data
-physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
+physicsClient = p.connect(p.DIRECT)#or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
-p.setGravity(0,0,-10)
-planeId = p.loadURDF("plane.urdf")
+p.setGravity(0,0,-10) # Default is no gravity!
+planeId = p.loadURDF("plane.urdf") # Use a Universal Robot Description File to load the robot 
 cubeStartPos = [0,0,1]
 cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
 boxId = p.loadURDF("r2d2.urdf",cubeStartPos, cubeStartOrientation)
