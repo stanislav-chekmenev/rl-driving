854524a64813a34887babcb9a3c077fd8cd75a47
stanislav-chekmenev <stanislav.chekmenev@gmail.com>
2020-02-15 20:14:24 +0100

    package made

--- install_requirements.sh
+++ install_requirements.sh
@@ -2,14 +2,10 @@
 
 conda create --name rl_env python=3.6  
 conda init bash
-conda activate rl_env 
+conda activate rld_env 
 git clone https://github.com/openai/gym.git
 cd gym 
 pip install -e . 
-cd .. 
-git clone https://github.com/benelot/pybullet-gym.git 
-cd pybullet-gym 
-pip install -e .
 cd .. 
 pip install -r requirements.txt
 printf "********************************** \n  The installation is successful\n  Please activate now the environment, using conda activate rl_env \n**********************************\n"
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rldriving',
+    version='0.0.1',
+    author="Dr. Stanislav Chekmenev",
+    author_email="stanislav.chekmenev@gmail.com",
+    description="World Models and PPO2 for gym_metacar",
+    long_description="World Models and PPO2 for gym_metacar",
+    long_description_content_type="text/markdown",
+    url="https://github.com/stanislav-chekmenev/rl-driving",
+    install_requires=[],
+    packages=find_packages()
+)
